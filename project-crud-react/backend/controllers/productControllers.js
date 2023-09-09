102799c5ca5527c367843cc35c038fc893a662a5
Ana Qonitah Munawwaroh <75164626+anaqonitah27@users.noreply.github.com>
2023-09-09 17:09:14 +0700

    add router getProductById and createProduct

--- project-crud-react/backend/controllers/productControllers.js
+++ project-crud-react/backend/controllers/productControllers.js
@@ -8,3 +8,25 @@ export const getProduct = async(req,res) =>{
         console.log(error.message);
     }
 }
+export const getProductById = async(req,res) =>{
+    try {
+        const response = await Product.findOne({
+            where:{
+                id: req.params.id
+            }
+        })
+
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createProduct = async(req,res) =>{
+    try {
+        await Product.create(req.body);
+        res.status(200).json("User Created");
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- project-crud-react/backend/routes/productRoutes.js
+++ project-crud-react/backend/routes/productRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {getProduct} from "../controllers/productControllers.js";
+import {createProduct, getProduct, getProductById} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.get('/getproducts', getProduct);
+router.get('/getproducts/:id', getProductById);
+router.post('/getproducts', createProduct);
 
 export default router;
